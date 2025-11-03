c1d90c599bb01f8e032f27a133e0ac9fcb950441
MINDOR-TV <65174562+MINDOR-TV@users.noreply.github.com>
2025-11-03 10:54:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -47,10 +47,10 @@
 const menuData = {
   "Hlavní stránka": "https://mindor-tv.github.io/popel_trisiasu/index.html",
   "Postavy": [
-    { name: "Koblížkova postava", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/koblizek/character.html" },
-    { name: "Eminky postava", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/eminka/character.html" },
-    { name: "Fílova postava", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/filip/character.html" },
-    { name: "Kájina postava", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/kaja/character.html" },
+    { name: "Stogorin", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/stogorin/character.html" },
+    { name: "Káji Š. postava", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/kaja/character.html" },
+    { name: "Bogdan", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/bogdan/character.html" },
+    { name: "Ofélie", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/ofelie/character.html" },
     { name: "Žandy postava", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/zanda/character.html" },
     { name: "Tobyho postava", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/toby/character.html" }
   ],
