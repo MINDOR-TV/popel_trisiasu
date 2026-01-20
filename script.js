01dead84cea116c29d43ea4cd2f39a92c501ebce
MINDOR-TV <65174562+MINDOR-TV@users.noreply.github.com>
2026-01-20 13:25:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -48,10 +48,10 @@ const menuData = {
   "Hlavní stránka": "https://mindor-tv.github.io/popel_trisiasu/index.html",
   "Družina": [
     //{ name: "Stogorin", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/stogorin/character.html" },
-    { name: "Celeana Dawnshield", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/celeana/character.html" },
-    { name: "Bogdan", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/bogdan/character.html" },
-    { name: "Ofélie", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/ofelie/character.html" },
     { name: "Aeriel", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/aeriel/character.html" },
+    { name: "Bogdan", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/bogdan/character.html" },
+    { name: "Celeana Dawnshield", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/celeana/character.html" },
+    { name: "Ofélie", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/ofelie/character.html" }, 
     { name: "Thalor", url: "https://mindor-tv.github.io/popel_trisiasu/postavy/thalor/character.html" }
   ],
   "Mapy": [
